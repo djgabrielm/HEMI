9f76ef572ed4cced886dbaa646ddaa1b19745b1e
DrWatson <no@mail>
2021-04-09 14:19:46 -0600

    Carga de datos de Nicaragua

---
+++ scripts/load_data_nica.jl
@@ -0,0 +1,22 @@
+using DrWatson
+@quickactivate "HEMI"
+
+## Cargar datos de la base 2006 
+using Dates, CSV, DataFrames
+using CPIDataBase
+
+# Base 2006 que empieza en dic-09
+nic_base06 = CSV.read(datadir("nicaragua", "Nicaragua_IPC_2006.csv"), 
+    DataFrame, normalizenames=true)
+nic06gb = CSV.read(datadir("nicaragua", "Nicaragua_GB_2006.csv"), 
+    DataFrame, types=[String, String, Float64])
+
+full_nic06 = FullCPIBase(nic_base06, nic06gb)
+nic06 = VarCPIBase(full_nic06)
+
+## Guardar datos para su carga posterior
+using JLD2
+
+@save datadir("nicaragua", "nicadata.jld2") nic06
+
+nicadata = CountryStructure((nic06,))
